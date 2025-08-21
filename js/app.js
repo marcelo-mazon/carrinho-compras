17ee5dd478c0ab8d4a1ac73d4ab14b1fb021a004
Marcelo Mazon <marcelo.tmazon@gmail.com>
2025-08-21 19:17:41 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+let valorTotal; // Variável global para armazenar o valor total
+limpar();
+
+function adicionar() {
+    // Obter o valor do produto selecionado
+    const listaDeProdutos = document.getElementById('produto');
+    const produtoSelecionado = listaDeProdutos.value;
+
+    // Separar o valor em nome do produto e preço
+    const [nomeProduto, precoProduto] = produtoSelecionado.split(' - R$');
+    const preco = Number(precoProduto); // Converter preço para número, ex.: 1400
+
+    // Obter a quantidade
+    const entrada = document.getElementById("quantidade");
+    const quantidade = Number(entrada.value); // Converter quantidade para número
+
+    // Validar a quantidade
+    if (quantidade <= 0) {
+        alert("Por favor, insira uma quantidade válida (maior que 0)");
+        return;
+    }
+
+    
+    // Adicionar o produto ao carrinho
+    const carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+    
+
+    // Atualizar o valor total
+    valorTotal += quantidade * preco; // Somar o valor do novo produto
+    const elementoTotal = document.getElementById("valor-total");
+    elementoTotal.textContent = `R$${valorTotal}`; // Atualizar o elemento com o novo total
+
+    // Limpar o campo de quantidade
+    entrada.value = "";
+
+}
+
+function limpar() {
+    valorTotal = 0;
+    document.getElementById("lista-produtos").innerHTML = "";
+    document.getElementById("valor-total").textContent = "R$0";
+
+    // Limpar o campo de quantidade
+    document.getElementById("quantidade").value = "";
+}
